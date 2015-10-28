c1897782e47934dc160afadc6652037337d9091f
snerpent <snerpent17@gmail.com>
2015-10-28 16:39:41 -0700

    Revised Lesson 15

--- script15.js
+++ script15.js
@@ -2,4 +2,8 @@ $(document).ready(function() {
 //	$('#A, #B').something(colorThis);
 	$('*').css('background', 'yellow');
 	$('.myDiv:contains(jQuery)').css('background', 'white');
+	console.log($('span[data-xyz]').html());
+	$('a[href^="https"]').css('background', 'black').css('color', 'white');
+	$('span[class$= "abc"]').css('background', 'black').css('color', 'white');
+	$('div:not(.abcd)').css('color', 'red');
 });
